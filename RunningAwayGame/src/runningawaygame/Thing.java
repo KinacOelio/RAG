4321a479dd38e4354f932516f40bc39a5e971cb3
KinacOelio <47313425+KinacOelio@users.noreply.github.com>
2019-02-06 17:52:47 -0500

    Add files via upload

--- RunningAwayGame/src/runningawaygame/Thing.java
+++ RunningAwayGame/src/runningawaygame/Thing.java
@@ -5,6 +5,7 @@ package runningawaygame;
 import static runningawaygame.RunningAwayGame.*;
 
 public class Thing {
+    private int index;
     private boolean stolen = false;
     protected String name = "default name";
     private String sPlace = "default place";
@@ -22,23 +23,22 @@ public class Thing {
         //c<cost
         //
     
-    public Thing(String thingString){
-if(RunningAwayGame.debug){System.out.println("thingString: " + thingString);} 
+    public Thing(String thingString, int index){
+        this.index = index;
+//if(RunningAwayGame.debug){System.out.println("thingString: " + thingString);} 
         if(thingString.contains("n>")){
          int a = thingString.indexOf("n>");
          int b = thingString.substring(a).indexOf('\n');
-if(RunningAwayGame.debug){System.out.println("thisb: " + b);}  
+//if(RunningAwayGame.debug){System.out.println("thisb: " + b);}  
          String thisName = thingString.substring(a+2, b).trim();
-if(RunningAwayGame.debug){System.out.println("thisName: " + thisName);}        
+//if(RunningAwayGame.debug){System.out.println("thisName: " + thisName);}        
          this.name = thisName;
     }
- if(thingString.contains("$tr")){
-if(RunningAwayGame.debug){System.out.println("thisb: " + b);}  
-         String thisName = thingString.substring(a+2, b).trim();
-if(RunningAwayGame.debug){System.out.println("thisName: " + thisName);}        
-         this.name = thisName;
-        
-    }
+
+ 
+}//end constructor
+//-------------
+    
     public String getName(){return name;}
     public Place getLoc(){return location;}
     
